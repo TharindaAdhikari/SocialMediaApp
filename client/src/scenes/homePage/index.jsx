380ded3c70969373a904def37c6a305b85f9839a
TharindaAdhikari <it19047966@my.sliit.lk>
2023-07-08 19:55:04 +0530

    all add

---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,48 @@
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
+import NavBar from "scenes/navbar";
+import UserWidget from "scenes/widget/UserWidget";
+import MyPostWidget from "scenes/widget/MyPostWidget";
+import PostsWidget from "scenes/widget/PostsWidget";
+import AdvertWidget from "scenes/widget/AdvertWidget";
+import FriendListWidget from "scenes/widget/FriendListWidget";
+
+const HomePage = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  //
+  const data = useSelector((state) => state.user);
+  const { _id, picturePath } = data || {};
+  //
+  return (
+    <Box>
+      <NavBar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        >
+          <MyPostWidget picturePath={picturePath} />
+          <PostsWidget userId={_id} />
+        </Box>
+        {isNonMobileScreens && (
+          <Box flexBasis="26%">
+            <AdvertWidget />
+            <Box m="2rem 0" />
+            <FriendListWidget userId={_id} />
+          </Box>
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+export default HomePage;
